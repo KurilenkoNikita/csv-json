7254d668bdf1dd138b19a3d99297baa1b8bf2814
Kurilenko Nikita <cool.nik.007@yandex.ru>
2024-02-18 17:55:20 +0300

    получилось перевести json в csv

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-import pandas as p
-print('Imported')
+import pandas as pd
+from pathlib import Path
+import json
+
+# file is in current dir
+path = Path(r'calendar.json')
+
+# read json
+with path.open('r', encoding='utf-8') as f:
+    data = json.loads(f.read())
+
+# create dataframe
+df = pd.json_normalize(data)
+
+# save to csv
+df.to_csv('test.csv', index=False, encoding='utf-8')  
+
